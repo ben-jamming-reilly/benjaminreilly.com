3f03b61abb36226ce20fe3d1261cd2f3515d3be7
Ben Reilly <benreilly0809@gmail.com>
2024-02-08 10:39:06 -0800

    really candid add

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,18 +1,16 @@
 const projectsData = [
   {
+    title: "Really Candid",
+    description: `An app for really honest 360° feedback. It enables people to see their growth overtime with anonymous, AI-powered feedback.`,
+    imgSrc: "/static/images/really-candid-heart.png",
+    href: "https://reallycandid.com/",
+  },
+  {
     title: "parasocial",
     description: `a way to discover and relive memorable moments from your favorite content creators`,
     imgSrc: "/static/images/parasocial-home.png",
     href: "https://parasocial.online/",
   },
-  // {
-  //   title: 'The Time Machine',
-  //   description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-  //   to the desired date and press "Go". No more worrying about lost keys or
-  //   forgotten headphones with this simple yet affordable solution.`,
-  //   imgSrc: '/static/images/time-machine.jpg',
-  //   href: '/blog/the-time-machine',
-  // },
 ];
 
 export default projectsData;
