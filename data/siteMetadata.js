56072445d7f3c3b16c5aa33fd1f12edb2668eced
Ben Reilly <benreilly0809@gmail.com>
2025-05-14 14:11:40 -0700

    improved content

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -9,7 +9,7 @@ const siteMetadata = {
   siteUrl: "https://benjaminreilly.com",
   siteRepo: "https://github.com/ben-jamming-reilly/benjaminreilly.com",
   siteLogo: "/static/images/logo.png",
-  socialBanner: "/static/images/twitter-card.png",
+  socialBanner: "/static/images/tomorrowland.jpg",
   // mastodon: "https://mastodon.social/@mastodonuser",
   email: "benreilly0809@gmail.com",
   github: "https://github.com/ben-jamming-reilly",
