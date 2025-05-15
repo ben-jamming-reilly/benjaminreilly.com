4d9b542f9fa921ef1954809708fa2a335940f39c
Ben Reilly <benreilly0809@gmail.com>
2025-05-14 18:56:44 -0700

    adjusting the favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,10 +75,9 @@ export default function RootLayout({
       className={`${courier_prime.variable} ${jost.variable} font-serif scroll-smooth`}
       suppressHydrationWarning
     >
-      <link
-        rel="icon"
-        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>✌</text></svg>"
-      />
+      <link rel="icon" type="image/png" href="/static/images/logo.png" />
+      <link rel="apple-touch-icon" href="/static/images/logo.png" />
+      <link rel="shortcut icon" href="/static/images/logo.png" />
 
       <meta name="msapplication-TileColor" content="#000000" />
       <meta
